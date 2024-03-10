f1ba24c96778d1cde23642dfbd3129ee4c478775
bsroawit1234 <sorawit.pha@student.mahidol.edu>
2024-03-10 15:38:07 +0700

    final

--- src/main/java/Project1_6513122/Main.java
+++ src/main/java/Project1_6513122/Main.java
@@ -1,8 +1,11 @@
 package Project1_6513122;
+// Sorawit Phattharakundilok    6513122
+// Thinnaphat Phumphotingam     6513166
+// Wish Semangern               6513175
+// Napasrapee Satittham         6513012
 
 import java.io.*;
 import java.util.*;
-
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/Project1_6513122/MyException.java
+++ src/main/java/Project1_6513122/MyException.java
@@ -1,5 +1,10 @@
 package Project1_6513122;
 
+// Sorawit Phattharakundilok    6513122
+// Thinnaphat Phumphotingam     6513166
+// Wish Semangern               6513175
+// Napasrapee Satittham         6513012
+
 public class MyException extends Exception {
     public MyException(String msg) {super(msg);}
 }
